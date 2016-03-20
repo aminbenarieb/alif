184de706dde3121cfbcc8b77ca25c05a213c8a33
aminbenarieb <amin.benarieb@gmail.com>
2016-03-20 22:28:07 +0300

    Started DragDrop feature (not working yet).

---
+++ Code/DragDropWord.swift
@@ -0,0 +1,26 @@
+//
+//  DragDropWord.swift
+//  Alif
+//
+//  Created by Amin Benarieb on 11/03/16.
+//  Copyright © 2016 Amin Benarieb. All rights reserved.
+//
+
+import Foundation
+import Material
+
+class DragDropWord : UIView
+{
+    
+    @IBOutlet var resultLabel: TextField!
+    @IBOutlet var wordCollection: UICollectionView!
+    
+    override func drawRect(rect: CGRect) {
+        
+        
+        
+        super.drawRect(rect)
+        
+    }
+    
+}
