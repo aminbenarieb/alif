83d57d6ecfe9a4f761f5d9eba1d1077ca8ed07f2
aminbenarieb <amin.benarieb@gmail.com>
2016-03-20 22:29:22 +0300

    Topic renamed to ModeChoice.

---
+++ Code/ModeChoice.swift
@@ -0,0 +1,46 @@
+//
+//  Topic.swift
+//  
+//
+//  Created by Amin Benarieb on 07/02/16.
+//
+//
+
+import Foundation
+import Material
+
+class ModeChoice: UIViewController
+{
+    
+    @IBOutlet var btnTrain : FlatButton!
+    @IBOutlet var btnLearn : FlatButton!
+    
+    
+    override func viewDidLoad() {
+        
+        for btn in [btnTrain, btnLearn]
+        {
+            btn.setTitleColor(.whiteColor(), forState: .Normal)
+            btn.pulseColor = MaterialColor.white
+        }
+    }
+    
+    @IBAction func pushToLearn()
+    {
+        //
+        //        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        //        let unlockScreen = storyBoard.instantiateViewControllerWithIdentifier("Learn")
+        //        self.navigationController?.pushViewController(unlockScreen, animated: true)
+    }
+    @IBAction func pushToTrain()
+    {
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        let unlockScreen = storyBoard.instantiateViewControllerWithIdentifier("Train")
+        
+        
+        self.navigationController?.pushViewController(unlockScreen, animated: true)
+    }
+
+    
+    
+}
